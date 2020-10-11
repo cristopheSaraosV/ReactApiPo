2126e7f8b713eb648535c106c64b3be1e6c135e9
cristopheSaraosV <46081150+cristopheSaraosV@users.noreply.github.com>
2020-10-10 21:32:12 -0500

    Add files via upload

---
+++ precache-manifest.d395c014ef279c5d798d9029c0213375.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aeb4dd493c494f4f78064a343929033",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d53d2e2d292392e4d30",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf0ade3371d1988a85df",
+    "url": "/static/js/2.4b70e8d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4b70e8d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d53d2e2d292392e4d30",
+    "url": "/static/js/main.0a064d90.chunk.js"
+  },
+  {
+    "revision": "6dbfa2daa40692075d8c",
+    "url": "/static/js/runtime-main.2df8a12b.js"
+  },
+  {
+    "revision": "6db5375049badafb8f88e36570dc74f6",
+    "url": "/static/media/Poke_Ball.6db53750.png"
+  }
+]);
